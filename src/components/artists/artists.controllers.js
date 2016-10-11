604a23d767e4949d4bbe71a166eeb2b3f4041fc4
khsagehorn <huntersagehorn@gmail.com>
2016-10-10 21:57:24 -0400

    implemented audio preview fuctionality

--- src/components/artists/artists.controllers.js
+++ src/components/artists/artists.controllers.js
@@ -21,3 +21,21 @@ spotifyApp.controller('artistSearchController', function($scope, $http){
   }
 
 })
+
+spotifyApp.controller('previewController', function($scope, $http){
+
+  var albumID;
+  $scope.artistID;
+
+  $scope.preview = function(id){
+    $http.get("https://api.spotify.com/v1/artists/"+id+"/top-tracks?country=US")
+    .success(function(data){
+      previewURL = data.tracks[0].preview_url;
+      console.log(data);
+      console.log(data.tracks[0].preview_url);
+      var audioObject = new Audio(data.tracks[0].preview_url);
+      audioObject.play();
+
+    })
+  }
+})
